591b6cc85287f44c63398c7709be72b3dc09f742
sanikemppainen <sani.kemppainen@helsinki.fi>
2022-06-29 17:29:54 +0300

    frontin build

---
+++ build/precache-manifest.26d4a185ff22b84ffb250f2466d060aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97679d7314592d1dfd26",
+    "url": "/static/css/main.15762968.chunk.css"
+  },
+  {
+    "revision": "97679d7314592d1dfd26",
+    "url": "/static/js/main.97679d73.chunk.js"
+  },
+  {
+    "revision": "42323de52940558adb9d",
+    "url": "/static/js/1.42323de5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5bc4770810af8c255b5a99a3886e81cf",
+    "url": "/index.html"
+  }
+];
