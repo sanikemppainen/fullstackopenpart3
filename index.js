b91f611cdcdff587f713f6db86aa349391db553d
sanikemppainen <sani.kemppainen@helsinki.fi>
2022-06-29 17:32:54 +0300

    frontend fix

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token("mdata", function(req, res){
     //const {body}=req
